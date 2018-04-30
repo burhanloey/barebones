c29aed61f000f2c51752a768a42b64d729e188ab
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-04-30 13:32:58 +0800

    Add Bootstrap CSS

--- src/clj/barebones/view.clj
+++ src/clj/barebones/view.clj
@@ -1,12 +1,13 @@
 (ns barebones.view
-  (:require [hiccup.page :refer [html5 include-js]]))
+  (:require [hiccup.page :refer [html5 include-js include-css]]))
 
 (defn admin-page [req]
   (html5
    [:head
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-    [:title "Admin Page"]]
+    [:title "Admin Page"]
+    (include-css "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css")]
    [:body
     [:h1 "Admin Dashboard"]
     [:div#app]
