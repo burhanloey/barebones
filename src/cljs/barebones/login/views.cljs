dc86cf2a46498e2d30482c1734349dc9b2cc8ac8
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-02 19:20:02 +0800

    Stylize login page

--- src/cljs/barebones/login/views.cljs
+++ src/cljs/barebones/login/views.cljs
@@ -1,5 +1,23 @@
 (ns barebones.login.views)
 
 (defn login-page []
-  [:div
-   [:p "Login Page"]])
+  [:div.col-md-4.mx-auto.mt-5
+   [:div.card
+    [:div.card-header "Sign in"]
+    [:div.card-body
+
+     [:form
+      [:div.form-group
+       [:input#username.form-control {:placeholder "Username" :type "text"}]]
+
+      [:div.form-group
+       [:input#password.form-control {:placeholder "Password" :type "password"}]]
+
+      [:div.form-group.form-check
+       [:input#remember-me.form-check-input {:type "checkbox"}]
+       [:label.form-check-label {:for "remember-me"} "Remember me"]]
+
+      [:button.btn.btn-success.btn-block
+       {:on-click (fn [evt]
+                    (.preventDefault evt))}
+       "Login"]]]]])
