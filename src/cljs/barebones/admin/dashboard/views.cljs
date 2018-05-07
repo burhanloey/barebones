9f9c804caa89c1ea3b40037f7467eb07551da83f
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-07 09:03:05 +0800

    Unwrap dashboard

--- src/cljs/barebones/admin/dashboard/views.cljs
+++ src/cljs/barebones/admin/dashboard/views.cljs
@@ -1,7 +1,7 @@
 (ns barebones.admin.dashboard.views)
 
 (defn status-card [{:keys [icon]} count text]
-  [:div.column.is-4
+  [:div.column.is-3
    [:div.box.has-text-white-ter.has-background-grey-dark
     [:div.columns
      [:div.column icon]
@@ -15,16 +15,14 @@
     [:h3.title.is-6.has-text-white-ter.is-uppercase "Issues"]
 
     [:div.columns
-     [:div.column.is-10
-      [:div.columns
-       [status-card
-        {:icon [:i.fas.fa-check.fa-5x]}
-        50 "Solved"]
+     [status-card
+      {:icon [:i.fas.fa-check.fa-5x]}
+      50 "Solved"]
 
-       [status-card
-        {:icon [:i.fas.fa-spinner.fa-5x]}
-        25 "In Progress"]
+     [status-card
+      {:icon [:i.fas.fa-spinner.fa-5x]}
+      25 "In Progress"]
 
-       [status-card
-        {:icon [:i.fas.fa-list-ul.fa-5x]}
-        5 "To Do"]]]]]])
+     [status-card
+      {:icon [:i.fas.fa-list-ul.fa-5x]}
+      5 "To Do"]]]])
