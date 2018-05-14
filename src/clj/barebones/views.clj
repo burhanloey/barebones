b8391c5e1173543e03b114dca28d08d66cbdbd92
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-14 12:05:53 +0800

    Add leaflet js (#12)
    * Add leaflet js
    
    * Add integrity

--- src/clj/barebones/views.clj
+++ src/clj/barebones/views.clj
@@ -3,8 +3,11 @@
 
 (defn include-js
   "Returns Hiccup view to include javascript file."
-  [src]
-  [:script {:src src}])
+  [src & [integrity]]
+  (let [attr {:src src}]
+    [:script (if integrity
+               (assoc attr :integrity integrity :crossorigin "anonymous")
+               attr)]))
 
 (defn include-css
   "Returns Hiccup view to link stylesheet file, i.e, <link> tag. Takes optional
@@ -23,12 +26,15 @@
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
     [:title "Admin Page"]
-    (include-css
-     "https://use.fontawesome.com/releases/v5.0.10/css/all.css"
-     "sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg")
+    (include-css "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+                 "sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==")
+    (include-css "https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+                 "sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg")
     (include-css "/css/style.css")]
    [:body
     [:div#app]
+    (include-js "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+                "sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==")
     (include-js "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js")
     (include-js "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js")
     (include-js "js/compiled/app.js")
--- src/cljs/barebones/admin/location/views.cljs
+++ src/cljs/barebones/admin/location/views.cljs
@@ -1,4 +1,30 @@
-(ns barebones.admin.location.views)
+(ns barebones.admin.location.views
+  (:require [reagent.core :as r]))
+
+(defn world-map []
+  (r/create-class
+   {:component-did-mount
+    (fn [this]
+      (let [l (-> js/L
+                  (.map (r/dom-node this))
+                  (.setView #js [4.848 100.737] 13))]
+        (-> js/L
+            (.tileLayer "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        #js {:attribution "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"})
+            (.addTo l))
+
+        (-> js/L
+            (.marker #js [4.848 100.737] 13)
+            (.bindPopup "Issue #54 reported here")
+            (.addTo l))))
+
+    :render
+    (fn []
+      [:div {:style {:height "480px"}}])}))
 
 (defn location-panel []
-  [:h1.border-bottom "Location"])
+  [:div
+   [:section.section
+    [:div.box.has-background-grey-dark
+     [:h3.title.is-6.has-text-white-ter.is-uppercase "Location"]
+     [world-map]]]])
