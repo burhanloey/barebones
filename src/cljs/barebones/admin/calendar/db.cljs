d7f432fe8c2f6c528c44a9c4005c060a967edcec
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-14 08:59:04 +0800

    Comment out year-chart data from default-db because React still needs to render it anyway, it takes quite a while.

--- src/cljs/barebones/admin/calendar/db.cljs
+++ src/cljs/barebones/admin/calendar/db.cljs
@@ -1,15 +1,15 @@
 (ns barebones.admin.calendar.db
   (:require [barebones.admin.calendar.utils :as utils]))
 
-(def default-heatmap-data
-  (utils/generate-calendar-heatmap-data
-   (utils/find-details-for-year-data {})))
+;; (def default-heatmap-data
+;;   (utils/generate-calendar-heatmap-data
+;;    (utils/find-details-for-year-data {})))
 
-(def default-months-legend
-  (utils/generate-months-legend-data default-heatmap-data))
+;; (def default-months-legend
+;;   (utils/generate-months-legend-data default-heatmap-data))
 
-(def default-db {:heatmap-data default-heatmap-data
-                 :months-legend default-months-legend
+(def default-db {;; :heatmap-data default-heatmap-data
+                 ;; :months-legend default-months-legend
                  :days-legend (utils/generate-days-legend)
                  :week-data (repeat 7 0)
                  :last-7-dates (map #(-> (js/moment)
